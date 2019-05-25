d64ff92bd0854454c3ea3989df9c4a9d5e893023
Louise P <cpieri@student.42.fr>
2019-05-25 09:21:20 +0200

    [Config]: FrontEnd config
    - add webpack.config.dev.jjs

---
+++ front/webpack.config.dev.js
@@ -0,0 +1,31 @@
+'use strict'
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/main.js'
+  ],
+  module: {
+    rules: [
+		{
+			test: /\.vue$/,
+			loader: 'vue-loader'
+		},
+		{
+			test: /\.js$/,
+			loader: 'babel-loader'
+		},
+		{
+			test: /\.css$/,
+			use: [
+				'vue-style-loader',
+				'css-loader'
+			]
+		}
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
