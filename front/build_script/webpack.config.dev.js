68a3028374e64ea5f852e797ecb2c13700ba3c2e
Louise P <cpieri@student.42.fr>
2019-05-25 10:21:07 +0200

    [Config]: Config front

---
+++ front/build_script/webpack.config.dev.js
@@ -0,0 +1,47 @@
+'use strict'
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const HOST = 'localhost'
+const PORT = 8080
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/main.js'
+  ],
+  devServer: {
+    clientLogLevel: 'warning',
+    hot: true,
+    contentBase: 'dist',
+    compress: true,
+    host: HOST,
+    port: PORT,
+    overlay: { warnings: false, errors: true },
+    publicPath: '/',
+    quiet: true
+  },
+  module: {
+    rules: [
+		{
+			test: /\.vue$/,
+			loader: 'vue-loader'
+		},
+		{
+			test: /\.css$/,
+			use: [
+				'vue-style-loader',
+				'css-loader'
+			]
+		}
+    ]
+  },
+  plugins: [
+	new VueLoaderPlugin(),
+	new HtmlWebpackPlugin({
+		filname: 'index.html',
+		template: 'index.html',
+		inject: true
+	})
+  ]
+}
