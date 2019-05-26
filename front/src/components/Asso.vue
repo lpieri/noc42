d35eab8774450d253f0dab0d378f8e40e0358146
Louise P <cpieri@student.42.fr>
2019-05-26 03:20:31 +0200

    lol 9

--- front/src/components/Asso.vue
+++ front/src/components/Asso.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="section">
-    <div id="bandAsso" class="container card">
-      <figure class="image is-128x128">
-        <img src="../assets/passerelles17.png" alt="passerelles17" class="is-rounded">
-      </figure>
+    <div class="container">
+      <div id="bandAsso" class="columns card">
+        <figure id="picAssoFig" class="column image">
+          <img id="pictureAsso" :src="assoPicturePath" :alt="assoName"></img>
+        </figure>
+        <h1 id="assoName" class="column title is-1">{{ assoName }}</h1>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AssoProfile',
+  data () {
+    return {
+      assoName: 'Passerelles 17',
+      assoPicturePath: './assets/passerelles17.png'
+    }
+  }
+}
+</script>
 
 <style>
 #bandAsso {
   padding: 21px;
   border-radius: 10px;
+  align-items: center;
+  justify-content: center;
+}
+#picAssoFig {
+  max-width: min-content;
+}
+#pictureAsso {
+  width: 150px;
+  height: 150px;
+  background: no-repeat center;
+  background-size: contain;
+}
+#assoName {
+  min-width: max-content;
 }
 </style>
